7a058e75b4f455929ce1bdddb10524cce245acfa
Matthew Morgan <mail.matt.morgan@gmail.com>
2017-06-06 10:26:54 -0400

    Initial commit

---
+++ test.py
@@ -0,0 +1,29 @@
+import maze_solver
+import unittest
+
+
+SIMPLE_MAZE = [
+    ['S', '0', '1'],
+    ['1', '0', '1'],
+    ['1', '0', 'E']
+]
+
+
+MODERATE_MAZE = [
+    ['S', '0', '0', '1', 'E'],
+    ['1', '0', '1', '1', '0'],
+    ['1', '0', '0', '1', '0'],
+    ['0', '0', '1', '0', '0'],
+    ['1', '0', '0', '0', '0']
+]
+
+
+class TestMaze(unittest.TestCase):
+
+    def test_can_haz_maze(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
